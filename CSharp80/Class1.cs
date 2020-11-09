06f8e47468bfb44979c2024077640a285cf19d08
Nogic <24802730+nogic1008@users.noreply.github.com>
2020-11-09 16:46:41 +0900

    feat: add value class (#2)

--- CSharp80/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CSharp80
-{
-    public class Class1
-    {
-    }
-}
---
+++ CSharp80/ImmutableValueClass.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace CSharp80
+{
+    public class ImmutableValueClass : IEquatable<ImmutableValueClass>, IComparable, IComparable<ImmutableValueClass>
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public string? Remarks { get; }
+        public int? ParentId { get; }
+
+        public ImmutableValueClass(int id, string name, string? remarks = null, int? parentId = default)
+            => (Id, Name, Remarks, ParentId) = (id, name ?? throw new ArgumentNullException(nameof(name)), remarks, parentId);
+
+        public bool Equals(ImmutableValueClass? other)
+            => !(other is null)
+            && (Id, Name, Remarks, ParentId) == (other.Id, other.Name, other.Remarks, other.ParentId);
+
+        public override bool Equals(object? obj)
+            => obj is ImmutableValueClass other && Equals(other);
+
+        public override int GetHashCode()
+            => HashCode.Combine(Id, Name, Remarks, ParentId);
+
+        public override string ToString()
+            => $"{nameof(ImmutableValueClass)}: {{ {nameof(Id)}: {Id}, {nameof(Name)}: {Name}, {nameof(Remarks)}: {Remarks}, {nameof(ParentId)}: {ParentId} }}";
+
+        public void Deconstract(out int id, out string name, out string? remarks, out int? parentId)
+            => (id, name, remarks, parentId) = (Id, Name, Remarks, ParentId);
+
+        public int CompareTo(object? obj)
+            => obj is null ? 1
+            : obj is ImmutableValueClass other ? CompareTo(other)
+            : throw new ArgumentException(nameof(obj) + " is not a " + nameof(ImmutableValueClass), nameof(obj));
+
+        public int CompareTo(ImmutableValueClass? other)
+            => other is null ? 1 : Id - other.Id;
+
+        public static bool operator > (ImmutableValueClass? left, ImmutableValueClass? right)
+            => (left, right) switch
+            {
+                (ImmutableValueClass l, ImmutableValueClass r) => l.Id > r.Id,
+                ({}, null) => true,
+                (null, {}) => false,
+                (null, null) => false
+            };
+
+        public static bool operator < (ImmutableValueClass? left, ImmutableValueClass? right)
+            => (left, right) switch
+            {
+                (ImmutableValueClass l, ImmutableValueClass r) => l.Id < r.Id,
+                ({}, null) => false,
+                (null, {}) => true,
+                (null, null) => false
+            };
+
+        public static bool operator >= (ImmutableValueClass? left, ImmutableValueClass? right)
+            => (left, right) switch
+            {
+                (ImmutableValueClass l, ImmutableValueClass r) => l.Id >= r.Id,
+                ({}, null) => true,
+                (null, {}) => false,
+                (null, null) => true
+            };
+
+        public static bool operator <= (ImmutableValueClass? left, ImmutableValueClass? right)
+            => (left, right) switch
+            {
+                (ImmutableValueClass l, ImmutableValueClass r) => l.Id <= r.Id,
+                ({}, null) => false,
+                (null, {}) => true,
+                (null, null) => true
+            };
+    }
+}
